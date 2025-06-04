5d4425906c684abb390a3d1dc0dcbb18a52a7702
longvo34 <vothanhlong0987@gmail.com>
2025-06-04 11:48:04 +0700

    profile, appointment, loginform

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "@/common/Navbar";
 import routes from "@/routes/routes";
+import Footer from "./common/Footer";
+
 
 function App() {
   return (
     <div className ='mx-4 sm:mx-[10%]'>
       <Navbar />
       {routes}
+      <Footer />
     </div>
   );
 }
--- web-app/src/routes/routes.jsx
+++ web-app/src/routes/routes.jsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "@/pages/Home/HomePage";
 import Doctors from "@/pages/Doctors/Doctors";
+import AboutUs from "@/common/About";
+import Contact from "@/common/Contact";
+import LoginForm from "@/features/auth/components/LoginForm";
+import MyProfile from "@/features/auth/components/pages/UserProfile";
+import MyAppointments from "@/pages/Appointments/Appointments";
 const routes = (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/doctors" element={<Doctors />} />
     <Route path="/doctors/:speciality" element={<Doctors />} />
+    <Route path="/about" element={<AboutUs />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<LoginForm />} />
+    <Route path="/profile" element={<MyProfile/>} />
+    <Route path="/my-appointments" element={<MyAppointments/>} />
   </Routes>
 );
 export default routes;
