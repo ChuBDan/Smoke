95df577212a53db6d6a56f23af9c0e508e3c0b7e
TienNM <ojtgrowth@sendo.vn>
2025-06-27 14:29:37 +0700

    toast

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,7 +1,26 @@
 import routes from "@/routes/routes";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
-  return <div>{routes}</div>;
+  return (
+    <div>
+      {routes}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+      ;
+    </div>
+  );
 }
 
 export default App;
