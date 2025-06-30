9922e12d4589b5e778480a18d164d70fc75df6a9
ThangVVSE180202 <ThangVVSE180202@fpt.edu.vn>
2025-06-30 16:51:51 +0700

    Update dashboard sidebar icon SVG
    Replaces the dashboard icon SVG in the admin sidebar with a new version for improved appearance and consistency.

--- web-app/src/features/admin/components/Sidebar/index.jsx
+++ web-app/src/features/admin/components/Sidebar/index.jsx
@@ -61,25 +61,33 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
       path: "/admin",
       label: "Dashboard",
       icon: (
-        <svg
-          width="20"
-          height="20"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2-2z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 5a2 2 0 012-2h4a2 2 0 012 2v0a2 2 0 01-2 2H10a2 2 0 01-2-2z"
-          />
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M12 12C12 11.4477 12.4477 11 13 11H19C19.5523 11 20 11.4477 20 12V19C20 19.5523 19.5523 20 19 20H13C12.4477 20 12 19.5523 12 19V12Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></path>
+            <path
+              d="M4 5C4 4.44772 4.44772 4 5 4H8C8.55228 4 9 4.44772 9 5V19C9 19.5523 8.55228 20 8 20H5C4.44772 20 4 19.5523 4 19V5Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></path>
+            <path
+              d="M12 5C12 4.44772 12.4477 4 13 4H19C19.5523 4 20 4.44772 20 5V7C20 7.55228 19.5523 8 19 8H13C12.4477 8 12 7.55228 12 7V5Z"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></path>
+          </g>
         </svg>
       ),
     },
